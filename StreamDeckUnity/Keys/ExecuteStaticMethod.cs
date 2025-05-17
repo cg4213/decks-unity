80f099864f68537b56dac0efc0268b620bd82ca1
huayu.zhang <cg4213@aliyun.com>
2025-05-17 21:11:28 +0800

    增加执行静态方法接口

---
+++ StreamDeckUnity/Keys/ExecuteStaticMethod.cs
@@ -0,0 +1,44 @@
+﻿using BarRaider.SdTools;
+using Newtonsoft.Json;
+
+namespace StreamDeckUnity.Keys
+{
+    [PluginActionId("com.nicollasr.streamdeckunity.executestaticmethod")]
+    public class ExecuteStaticMethod : Key<ExecuteStaticMethodSettings>
+    {
+        public ExecuteStaticMethod(SDConnection connection, InitialPayload payload) : base(connection, payload)
+        {
+        }
+
+        public override void KeyPressed(KeyPayload payload)
+        {
+            base.KeyPressed(payload);
+
+            var pid = GetForeground().processId;
+
+            if (IsUnityForeground(pid))
+            {
+                MessageServer.Send(pid.ToString(), new ExecuteStaticMethodMessage(settings.TypeName, settings.MethodName));
+            }
+        }
+    }
+
+    public class ExecuteStaticMethodMessage : Message
+    {
+        public string TypeName { get; }
+        public string MethodName { get; }
+        public ExecuteStaticMethodMessage(string typeName, string methodName)
+        {
+            TypeName = typeName;
+            MethodName = methodName;
+        }
+    }
+
+    public class ExecuteStaticMethodSettings : KeySettings
+    {
+        [JsonProperty("typename")]
+        public string TypeName { get; set; }
+        [JsonProperty("methodname")]
+        public string MethodName { get; set; }
+    }
+}
